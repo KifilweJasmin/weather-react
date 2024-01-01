55d9597289c55d34896536c8f287d3a5eeef26f4
KifilweJasmin <137528030+KifilweJasmin@users.noreply.github.com>
2024-01-01 19:23:08 +0200

    Added a search engine

--- src/Weather.js
+++ src/Weather.js
@@ -2,4 +2,25 @@ import React from "react";
 import axios from "axios";
 import Loader from "react-loader-spinner";
 
-export default function Weather(props) {}
+export default function Weather(props) {
+  return (
+    <div className="Weather">
+      <form id="search-form" class="mb-3">
+        <div class="row">
+          <div class="col-9">
+            <input
+              type="search"
+              placeholder="Enter your city..."
+              class="form-control"
+              id="city-input"
+              autocomplete="off"
+            />
+          </div>
+          <div class="col-3">
+            <input type="submit" value="Search" class="btn btn-primary w-100" />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
