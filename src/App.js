9e4041ac8dc24ae8a95ce1dc5ea80ae5ee7ef076
KifilweJasmin <137528030+KifilweJasmin@users.noreply.github.com>
2024-01-01 19:19:23 +0200

    Added structure and code to App.js and removed code from Weather.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,61 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Hello World </h2>
-        <Weather city="Paris" />
-      </header>
-    </div>
+    <body>
+      <div class="container">
+        <div class="weather-app-wrapper">
+          <div class="weather-app">
+            <Weather />
+            <div class="overview">
+              <h1 id="city"></h1>
+              <ul>
+                <li>
+                  Last updated: Tuesday 10:00 <span id="date"></span>
+                </li>
+                <li id="description">Cloudy</li>
+              </ul>
+            </div>
+            <div class="row">
+              <div class="col-6">
+                <div class="d-flex weather-temperature">
+                  <img
+                    src="https://openweathermap.org/img/wn/04n@2x.png"
+                    alt="S"
+                    id="icon"
+                    class="float-left"
+                  />
+                  <div class="float-left">
+                    <strong id="temperature">19</strong>
+                    <span class="units"> °C </span>
+                  </div>
+                </div>
+              </div>
+              <div class="col-6">
+                <ul>
+                  <li>
+                    Humidity: <span id="humidity">80</span> %
+                  </li>
+                  <li>
+                    Wind: <span id="wind">10</span> km/h
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <small>
+            <a
+              href="https://github.com/KifilweJasmin/my-weather-app-updated"
+              target="_blank"
+            >
+              Open-source code
+            </a>
+            by KifilweJasmin
+          </small>
+        </div>
+      </div>
+
+      <script src="src/index.js"></script>
+    </body>
   );
 }
 
